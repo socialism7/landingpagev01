c0f4a1703267347734f7b1c2c19beee8cdc435d9
David Dunn <52668811+socialism7@users.noreply.github.com>
2019-07-09 00:09:06 +0700

    Add files via upload

---
+++ js/vendor/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Forbidden Area</title>
+
+<link href='../../img/ico.png' rel='shortcut icon'>
+</head>
+
+<body bgcolor="#000000">
+<center>
+<br />
+<br />
+<br />
+<br />
+<br />
+<img src="https://lh3.googleusercontent.com/up7vClR-XQeuhU_CO2IF1GvDFdC_zvOwDYCCgOsxpi-QDgE3x0YiKDpL6lOls6o9QV3q2W2I7ZxsaSK6pw=w192-h220-rw" />
+<br />
+<font face="Arial Black, Gadget, sans-serif" size="+3" color="#FFFFFF";>JANCOK . . . !</font>
+<br />
+<br />
+<font face="Courier New, Courier, monospace" size="+1" color="#009900";>please contact [at]ra.hidayatulloh</font>
+</center>
+</body>
+</html>
